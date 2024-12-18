920afa645ad476036b500782bac89c0ccaa6abc4
pooja <pooja@brainysensei.com>
2024-12-18 02:12:11 -0800

    process

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./componets/Navbar";
 import HeroSection from "./componets/HeroSection";
 import AboutSection from "./componets/AboutSection";
+import ProcessSection from "./componets/ProcessSection";
 
 
 const App =() =>{
@@ -10,6 +11,8 @@ const App =() =>{
       <Navbar />
       <HeroSection   />
       <AboutSection />
+      <ProcessSection />
+      
       
      
     </div>
