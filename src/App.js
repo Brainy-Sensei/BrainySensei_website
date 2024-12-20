b7400df8f37292f5bc7424ae5f0412d96c66ebd8
pooja <pooja@brainysensei.com>
2024-12-19 21:32:12 -0800

    featureSection

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import HeroSection from "./componets/HeroSection";
 import AboutSection from "./componets/AboutSection";
 import ProcessSection from "./componets/ProcessSection";
 
+import ProductSection from "./componets/FeatureSection";
+import FeatureSection from "./componets/FeatureSection";
+
 
 const App =() =>{
   return(
     <div className="App">
       <Navbar />
-      <HeroSection   />
+      <HeroSection  />
       <AboutSection />
       <ProcessSection />
+      <FeatureSection />
       
       
      
